cae7ac5102f0db83ae9dc81c7d006bc9ae0c1fe4
sbayrak <34121748+sbayrak@users.noreply.github.com>
2021-12-20 20:54:45 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Realtime Notification
+
+This basic app demonstrates how a realtime notifications work in a simple way. e.g. when you get a "liked" notification from another user, the bell icon shows a notification (1) .
+
+# Production Links
+
+<!-- [Realtime-notification](https://googledocs-clone-sbayrak.netlify.app/) <br> <br>
+[![Netlify Status](https://api.netlify.com/api/v1/badges/4e7d09ba-10d0-4578-b593-c9d953e4747f/deploy-status)](https://app.netlify.com/sites/googledocs-clone-sbayrak/deploys) -->
+
+> The backend is deployed to Heroku, so frontend and backend are deployed on different cloud providers. You may use other providers that are able to deploy a Node.js application.
+
+<br>
+
+## Tech Stack
+
+- React.js
+- Socket.io
+
+<br>
+
+## Dependencies
+
+Since there are **_client_** and **_server_** folders, please refer to `package.json` file in both folders. <br>
+Therefore, please run `npm install` in both folders.
+
+<br>
+
+## Environment Variables
+
+There is no `.env` variables.
---
+++ client/src/components/navbar/Navbar.js
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from 'react';
+import './Navbar.css';
+import Message from '../../img/message.svg';
+import Settings from '../../img/settings.svg';
+
+const Navbar = ({ socket }) => {
+  const [notifications, setNotifications] = useState([]);
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    socket.on('getNotification', (data) => {
+      setNotifications((prev) => [...prev, data]);
+    });
+  }, [socket, notifications]);
+
+  console.log(notifications);
+
+  const displayNotification = ({ senderName, type }) => {
+    let action;
+    if (type === 1) {
+      action = 'liked';
+    } else if (type === 2) {
+      action = 'commented';
+    } else {
+      action = 'shared';
+    }
+
+    return (
+      <span className='notification'>{`${senderName} ${action} your post.`}</span>
+    );
+  };
+
+  const handleRead = () => {
+    setNotifications([]);
+    setOpen(false);
+  };
+
+  return (
+    <div className='navbar'>
+      <div className='logo'>NotifyApp</div>
+      <div className='icons'>
+        <div className='icon' onClick={() => setOpen(!open)}>
+          <img src='./notification.svg' alt='asd' className='iconImg' />
+          {notifications.length > 0 && (
+            <div className='counter'>{notifications.length}</div>
+          )}
+        </div>
+        <div className='icon' onClick={() => setOpen(!open)}>
+          <img src={Message} alt='asd' className='iconImg' />
+        </div>
+        <div className='icon'>
+          <img src={Settings} alt='asd' className='iconImg' />
+        </div>
+      </div>
+      {open && (
+        <div className='notifications'>
+          {notifications.map((not) => displayNotification(not))}
+          <button className='nButton' onClick={handleRead}>
+            Mark as read
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
